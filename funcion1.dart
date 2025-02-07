29ca0dde03bad4fbd8d3198b9c96a5271820f181
Cesar Najera <164533997+Cesar-Najera-1068@users.noreply.github.com>
2025-02-07 16:47:44 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// Funcion que no devuelve valor
+// Funcion saludar
+void saludar(){
+  print("Hola Mundo");
+}
+
+void escribeNombre(){
+  print("Cesar");
+}
+
+void main(){
+  // Cesar Najera Mat: 22308051281068
+  //Llamar a la funcion saludar
+  print("Cesar Najera Mat: 22308051281068");
+  print("Llamando a la funcion saludar");
+  saludar();
+  print("Llamando a la funcion escribeNombre");
+  escribeNombre();
+}
