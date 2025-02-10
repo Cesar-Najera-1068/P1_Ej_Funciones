a7f0c8270ce0ab69f66408cb111a91d9aeca6620
Cesar Najera <164533997+Cesar-Najera-1068@users.noreply.github.com>
2025-02-10 18:04:30 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,16 @@
+// Funciones creadas por el usuario
+void suma(int a, int b){
+  print("La suma de $a + $b es: ${a+b}");
+}
+
+void resta(int a, int b){
+  print("La resta de $a - $b es: ${a-b}");
+}
+
+// Funcion principal (Main)
+void main(){
+  print("Cesar Najera Mat: 22308051281068");
+  print("Llamando a la funcion suma y resta");
+  suma(5, 3);
+  resta(5, 3); 
+}
