5073d4b6c601e115101480afaafb0c91258dcae6
Cesar Najera <164533997+Cesar-Najera-1068@users.noreply.github.com>
2025-02-11 20:01:38 +0000

    ejemplo lista

---
+++ listas.dart
@@ -0,0 +1,28 @@
+void main(){
+  // Cesar Najera Mat: 22308051281068
+  
+  print("Cesar Najera Mat: 22308051281068");
+  List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
+  print(numeros);
+  print(numeros[0]);
+  // Listar los elementos de la lista con un ciclo for
+  for(int i = 0; i < numeros.length; i++){
+    print(numeros[i]);
+  }
+  // Lista tipo double con 5 elementos de estatura
+  List<double> estaturas = [1.70, 1.80, 1.60, 1.75, 1.65];
+  //Lista de 5 nombres de personas
+  List<String> nombres = ["Cesar", "Juan", "Maria", "Pedro", "Luis"];
+  //Imprimir lista de estaturas y nombres
+  for(int i = 0; i < estaturas.length; i++){
+    print("Estatura: ${estaturas[i]} Nombre: ${nombres[i]}");
+  
+  }
+
+
+
+  
+  
+
+
+}
